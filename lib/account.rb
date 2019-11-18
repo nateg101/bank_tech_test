7e57aa74b41888ad5d7501450d48a8de351a6f9e
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-11-18 18:38:33 +0000

    Added functions so that deposit requires date & transactions stored.

--- lib/account.rb
+++ lib/account.rb
@@ -1,14 +1,16 @@
 
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def initialize
     @balance = 0
+    @transactions = []
   end
 
-  def deposit(money)
+  def deposit(money, date)
     @balance = money + balance
+    @transactions << [money, date]
   end
 
 end
--- spec/make_deposit_spec.rb
+++ spec/make_deposit_spec.rb
@@ -3,7 +3,13 @@ require 'account'
 describe " making a deposit" do
   it "should update the balance correctly" do
     account = Account.new
-    account.deposit(500)
+    account.deposit(500, "18/11/19")
     expect(account.balance).to eq 500
+  end
+
+  it "can store a date when a deposit is made" do
+    account = Account.new
+    account.deposit(500, "18/11/19")
+    expect(account.transactions).to eq [[500, "18/11/19"]]
   end
 end
