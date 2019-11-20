9aeee2d807ac20866432d3e5a2ddf4d944260657
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-11-20 09:17:24 +0000

    Consolidated and refactored tests.

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,8 +4,23 @@ describe Account do
 
   subject(:account) { described_class.new }
 
-  describe 'making a withdrawal' do
+  describe '#deposit' do
+    it 'should update the balance correctly' do
+      account.deposit(500, '18/11/19')
+      expect(account.balance).to eq 500
+    end
 
+    it 'can store a date when a deposit is made' do
+      account.deposit(500, '18/11/19')
+      expect(account.transactions).to eq [{ :deposit => ['18/11/19', 500, 500] }]
+    end
+
+    it 'raises an error message if trying to make a deposit >= 0' do
+      expect(account.deposit(-10, '18/11/19')).to eq 'Your deposit must be more than 0.'
+    end
+  end
+
+  describe '#withdrawal' do
     it 'should update the balance correctly' do
       account.deposit(500, "18/11/2019")
       account.withdrawal(100, "19/11/2019")
